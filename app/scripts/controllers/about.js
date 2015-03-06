03e46cc53971c2f1be99f60349bf77bacdf4cacb
Peter Watters <peter.watters@fmr.com>
2015-03-06 12:15:13 +0000

    Comiting first scaffolded app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name casinoSiteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the casinoSiteApp
+ */
+angular.module('casinoSiteApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name casinoSiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the casinoSiteApp
+ */
+angular.module('casinoSiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
